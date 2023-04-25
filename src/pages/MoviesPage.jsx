aca12f74f641b3d1c8684c878c4de30848d6d7f1
RubenMeju <rubenmeju@outlook.es>
2023-04-25 21:47:35 +0200

    conditional titles

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,13 +1,20 @@
 import Paginator from '../components/Paginator'
 import Card from '../components/Card'
 import { useFetchMovies } from '../hooks/useFetchMovies'
+import { useLocation } from 'react-router-dom'
 
 export default function MoviesPage() {
   const { loading, error, movies } = useFetchMovies()
+  const { pathname } = useLocation()
+  console.log('fdsfds  ', pathname)
+
   return (
     <>
       <h1 className='text-3xl text-center font-bold  text-white'>
-        Popular Movies!
+        {pathname === '/' && 'Popular'}
+        {pathname === '/movies/now_playing' && 'En cartelera hoy'}
+        {pathname === '/movies/upcoming' && 'Proximanente'}
+        {pathname === '/movies/top_rated' && 'Mejor valoradas'}
       </h1>
       <div className='md: flex flex-wrap justify-between '>
         <Card loading={loading} error={error} data={movies} />
--- src/pages/TvPage.jsx
+++ src/pages/TvPage.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import Card from '../components/Card'
 import Paginator from '../components/Paginator'
 import { useFetchTV } from '../hooks/useFetchTV'
+import { useLocation } from 'react-router-dom'
 
 export default function TvPage() {
   const { loading, error, tv } = useFetchTV()
+  const { pathname } = useLocation()
+
   return (
     <>
       <h1 className='text-3xl text-center font-bold  text-white'>
-        Popular Tv!
+        {pathname === '/' && 'Popular'}
+
+        {pathname === '/tv/airing_today' && 'Se emite hoy'}
+        {pathname === '/tv/on_the_air' && 'En televisión'}
+        {pathname === '/tv/top_rated' && 'Mejor valoradas'}
       </h1>
       <div className='md: flex flex-wrap justify-between '>
         <Card loading={loading} error={error} data={tv} />
