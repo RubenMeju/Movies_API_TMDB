28696ed603ab996cf99782bf8b4117d6ad0e88c1
RubenMeju <rubenmeju@outlook.es>
2023-04-26 17:55:34 +0200

    styles lg:

--- src/pages/DetailsByIdPage.jsx
+++ src/pages/DetailsByIdPage.jsx
@@ -11,22 +11,52 @@ export default function DetailsByIdPage() {
   const id = params.id
 
   const { loading, error, data } = useFetchDetailsByID(pathAction, id)
+  console.log('data', data)
+  const title = data?.title ? data?.title : data?.name
   return (
     <>
       {loading && <CircleProgress />}
       {error && <h1>Algo ha salido mal</h1>}
 
-      <div className='text-slate-100'>
-        <h2 className='text-4xl text-center p-4'>{data?.title}</h2>
-        <img src={URL_IMAGE + data?.poster_path} alt={data?.title} />
+      <div className='w-full h-screen text-slate-100 lg:flex lg:justify-center lg:items-center'>
+        <div className='flex flex-col bg-green-500'>
+          <img src={URL_IMAGE + data?.poster_path} alt={title} />
+        </div>
 
-        <p>Presupuesto:</p>
-        <p>{data?.budget} $</p>
+        <div className='w-4/5 m-auto lg:p-10'>
+          <h2 className='text-4xl text-center p-4'>{title}</h2>
 
-        <p>V.O: {data?.original_language}</p>
+          <div className='pt-4 text-xl'>
+            {data?.budget && (
+              <div className='flex'>
+                <p className='text-slate-300 pr-2'>Presupuesto:</p>
+                <p>{data?.budget.toLocaleString('es-ES')} $</p>
+              </div>
+            )}
 
-        <p className='text-2xl text-center p-4'>Sipnosis</p>
-        <p className='w-4/5 m-auto'>{data?.overview}</p>
+            <div className='flex'>
+              <p className='text-slate-300 pr-2'>V.O:</p>
+              <p> {data?.original_language}</p>
+            </div>
+
+            {data?.number_of_episodes && data?.number_of_seasons && (
+              <>
+                <div className='flex'>
+                  <p className='text-slate-300 pr-2'>Temporadas:</p>
+                  <p>{data?.number_of_seasons}</p>
+                </div>
+
+                <div className='flex'>
+                  <p className='text-slate-300 pr-2'>Numero de episodios</p>
+                  <p>{data?.number_of_episodes}</p>
+                </div>
+              </>
+            )}
+          </div>
+
+          <p className='text-2xl text-center p-4'>Sipnosis</p>
+          <p className=' text-xl'>{data?.overview}</p>
+        </div>
       </div>
     </>
   )
