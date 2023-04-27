a7add89f276b18750353fb314ad02a7ec308f5ea
RubenMeju <rubenmeju@outlook.es>
2023-04-27 08:21:47 +0200

    .

--- src/services/movies.service.js
+++ src/services/movies.service.js
@@ -6,12 +6,13 @@ export const fetchMovies = async (
   category = 'popular',
   page = '1'
 ) => {
-  console.log('api env', import.meta.env.VITE_API_KEY)
+  console.log('api env impòrt', import.meta.env.VITE_API_KEY)
+  console.log('api env process', process.env.VITE_API_KEY)
   const res = await fetch(
     URL_API +
       URL_MOVIES_POPULAR +
       category +
-      import.meta.env.VITE_API_KEY +
+      process.env.VITE_API_KEY +
       '&language=' +
       language +
       '&page=' +
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  define: {
+    __APP_ENV__: process.env.VITE_API_KEY
+  },
+  plugins: [react()]
 })
